1d95240fa7879d4a38443e64beee540bac407932
hongcheol5125 <hongcheol6689@gmail.com>
2023-04-25 18:59:10 +0900

    코팩 강의 : 출첵 마지막 강의 1개 남음

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,30 +10,157 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool choolCheckDone = false;
+
   // latitude - 위도  ,  longitude - 경도
   static final LatLng companyLatLng = LatLng(
     35.122339541881,
     126.86703266041,
-  ); // 코팩 회사가 여의도여서 여의도 위,경도 가져옴
+  ); // 풍암고 위도, 경도 가져옴
 
   // 위치표시 변수
   static final CameraPosition initialPosition = CameraPosition(
     target: companyLatLng,
     zoom: 15, // 확대한 정도(Zoom Level)
   );
+
+  static final double okDisatance = 100;
+
+  static final Circle withinDistanceCircle = Circle(
+    circleId: CircleId('withinDistanceCircle'),
+    center: companyLatLng,
+    fillColor: Colors.blue.withOpacity(0.5),
+    radius: okDisatance,
+    strokeColor: Colors.blue, // 원 둘레 색깔
+    strokeWidth: 1, // 원 둘레를 몇픽셀 두께로 칠할건지
+  );
+
+  static final Circle notWithinDistanceCircle = Circle(
+    circleId: CircleId('notWithinDistanceCircle'),
+    center: companyLatLng,
+    fillColor: Colors.red.withOpacity(0.5),
+    radius: okDisatance,
+    strokeColor: Colors.red, // 원 둘레 색깔
+    strokeWidth: 1, // 원 둘레를 몇픽셀 두께로 칠할건지
+  );
+
+  static final Circle checkDoneCircle = Circle(
+    circleId: CircleId('checkDoneCircle'),
+    center: companyLatLng,
+    fillColor: Colors.green.withOpacity(0.5),
+    radius: okDisatance,
+    strokeColor: Colors.green, // 원 둘레 색깔
+    strokeWidth: 1, // 원 둘레를 몇픽셀 두께로 칠할건지
+  );
+
+  static final Marker marker = Marker(
+    markerId: MarkerId('marker'),
+    position: companyLatLng,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _RenderAppBar(
         appBar: AppBar(),
       ),
-      body: Column(
-        children: [
-          _CustomGoogleMap(initialPosition: initialPosition),
-          _ChoolCheckButton(),
-        ],
+      body: FutureBuilder<String>(
+        // FutureBuilder는 future를 return 해주는 함수만 받을 수 있다.
+        future: checkPermission(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          // FutureBuilder는 AsyncSnapshot snapshot도 받는다.
+          // print(snapshot.connectionState); --> snapshot 연결 상태를 프린트해줌(none[future함수 없을 때], waiting, active[streem에서만 사용], done 있음)
+          //   print(snapshot.data); --> print(snapshot.data); 하면 checkPermission 함수를 프린트 할 수 있다.
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data == '위치 권한이 허가되었습니다.') {
+            return StreamBuilder<Position>(
+              stream: Geolocator.getPositionStream(),
+              builder: (context, snapshot) {
+                bool isWithinRange = false;
+
+                if (snapshot.hasData) {
+                  final start =
+                      snapshot.data!; // hasData이면 null일 수가 없으므로 ! 붙여주자
+                  final end = companyLatLng;
+
+                  final distance = Geolocator.distanceBetween(
+                    start.latitude,
+                    start.longitude,
+                    end.latitude,
+                    end.longitude,
+                  );
+
+                  if (distance < okDisatance) {
+                    isWithinRange = true;
+                  }
+                }
+
+                return Column(
+                  children: [
+                    _CustomGoogleMap(
+                      initialPosition: initialPosition,
+                      circle: choolCheckDone
+                          ? checkDoneCircle
+                          : isWithinRange
+                              ? withinDistanceCircle
+                              : notWithinDistanceCircle,
+                      marker: marker,
+                    ),
+                    _ChoolCheckButton(
+                      isWithinRange: isWithinRange,
+                      choolCheckDone: choolCheckDone,
+                      onPressed: onChoolCheckPressed,
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+
+          return Center(
+            child: Text(snapshot.data),
+          );
+        },
       ),
     );
+  }
+
+  onChoolCheckPressed() async {
+    final result = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // AlertDialog
+          title: Text('출근하기'),
+          content: Text('출근을 하시겠습니까?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('출근하기'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (result) {
+      setState(() {
+        choolCheckDone = true;
+      });
+    }
   }
 
   // 위치권한 확인-Geolocator(모든 위치권한 받는 것들은 async로 작업해야한다! 왜냐면, 유저가 권한을 눌러주면 그때서야 권한가능하기때문에)
@@ -57,6 +184,7 @@ class _HomeScreenState extends State<HomeScreen> {
       }
     }
     if (checkedPermission == LocationPermission.deniedForever) {
+      // deniedForever는 한번 권한 취소하면 설정에 들어가서 직접 요청해야한다
       return '앱의 위치 권한을 설정에서 허가해주세요.';
     }
 
@@ -80,7 +208,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
 class _CustomGoogleMap extends StatelessWidget {
   final CameraPosition initialPosition;
-  const _CustomGoogleMap({required this.initialPosition, super.key});
+  final Circle circle;
+  final Marker marker;
+
+  const _CustomGoogleMap(
+      {required this.initialPosition,
+      required this.circle,
+      required this.marker,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -92,22 +227,56 @@ class _CustomGoogleMap extends StatelessWidget {
         // mapType: MapType.satellite, (위성지도)
         // mapType: MapType.terrain, (높낮이 나타나는 지도)
         initialCameraPosition: initialPosition, // 처음 카메라 위치
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        circles: Set.from([circle]),
+        markers: Set.from([marker]),
       ),
     );
   }
 }
 
 class _ChoolCheckButton extends StatelessWidget {
-  const _ChoolCheckButton({super.key});
+  final bool isWithinRange;
+  final VoidCallback onPressed;
+  final bool choolCheckDone;
+  const _ChoolCheckButton({
+    required this.isWithinRange,
+    required this.onPressed,
+    required this.choolCheckDone,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Text('출근'),
-    );
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.timelapse_outlined,
+          size: 50.0,
+          color: choolCheckDone
+              ? Colors.green
+              : isWithinRange
+                  ? Colors.blue
+                  : Colors.red,
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+        if (!choolCheckDone &&
+            isWithinRange) // 함수가 아닌 위젯에서 if문을 쓸 땐, if(){}가 아닌 if()로 씀
+          TextButton(
+            onPressed: onPressed,
+            child: Text('출근하기'),
+          ),
+      ],
+    ));
   }
 }
 
+// 굳이 class로 만들지 않고  appBar 메소드로 만들어서 쓰면 훨씬 더 간단해!!!!
 class _RenderAppBar extends StatelessWidget implements PreferredSizeWidget {
   final AppBar appBar;
 
@@ -124,6 +293,15 @@ class _RenderAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
       backgroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.my_location,
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 
